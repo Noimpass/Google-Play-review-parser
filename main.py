ef66c1c84001d28770bb3fce830756f1bcd5e347
0xJanus <blcklptn@icloud.com>
2024-08-21 12:37:49 +0500

    save

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import requests as rq
 from typing import List
+from google_play_scraper import app
+from google_play_scraper import Sort, reviews_all
 
 class ParseReviews:
     def __init__(self, links: List[str]):
         self.links = links
 
-    def _reconfigure_links(self):
-        for i in self.links:
-            
+    def scrape(self):
+        app_reviews = reviews_all(
+            'de.flaschpost.app', # GOOGLE APPLCIATION ID FROM LINK!
+            sleep_milliseconds=10,
+            lang='de',
+            country='de',
+            sort=Sort.NEWEST
+        )
+        print(app_reviews)
     
 
 def read_file(links_txt_path: str) -> List[str]:
@@ -21,8 +29,8 @@ def read_file(links_txt_path: str) -> List[str]:
 
 
 def main(links_txt_path: str) -> None: 
-    print(read_file(links_txt_path))
-
+    k = read_file(links_txt_path)
+    ParseReviews(k).scrape()
 
 if __name__ == '__main__':
     main('./links.txt')
